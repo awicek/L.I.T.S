6bf90a0903262027a76f3f6c130e11f5beab9268
Awicek <richard.rubas@email.cz>
2022-07-26 15:27:41 +0200

    zacinam to davat dokupy

---
+++ KVHRA/functions.py
@@ -0,0 +1,236 @@
+# DODELAT CREATE_RANDOM_BOARD
+import random
+
+
+class Stone ():
+    # must have for game:
+    # self.color
+    # self.max_x
+    # self.max_y
+    # self.matrix
+    # self.id
+
+    # others
+    # self.r0_180
+    # self.r90_270
+
+    def __init__(self,informace,id = 0):# DONE TESTED 
+        self.color, self.max_x, self.max_y, self.matrix = self.create(informace)
+        self.id = id
+        self.r0_180 = self.create_rotation1()
+        self.r90_270 =  self.create_rotation2()
+
+    def create (self, input):        # DONE TESTED 
+        color = input[0]
+        input2 = list()
+        for i in range(len(input[1:])//2):
+            input2.append([input[i*2+1], input[i*2+2]])
+
+        input2.sort()
+        x_min = input2[0][0]
+        x_max = input2[-1][0]
+
+        input2.sort(key = lambda x: x[1])
+
+        y_min = input2[0][1]
+        y_max = input2[-1][1]
+
+        x = x_max - x_min +1
+        y = y_max - y_min +1
+        matice = [[0 for _ in range(y)] for __ in range(x)]
+        
+        for i in input2:
+            matice[i[0]-x_min][i[1]-y_min] = color
+        return color ,len(matice),len(matice[0]),matice
+    
+    def create_rotation1 (self):    # DONE TESTED 
+        rotations = list()
+        r1 = list()
+        for i,j in enumerate(self.matrix):
+            for k,l in enumerate(j):
+                if l > 0:
+                    r1.append([i,k])
+        r2 = list()
+        for i in range(self.max_x):
+            for j in range(self.max_y):
+                if self.matrix[self.max_x - i -1][self.max_y - j -1] > 0:
+                    r2.append([i,j])
+        if r1 == r2:
+            rotations.append(r1)
+        else:    
+            rotations.append(r1)
+            rotations.append(r2)
+        return rotations
+
+    def create_rotation2 (self):    # DONE TESTED 
+        rotations = list()
+        r2 = list()
+        r1 = list()
+        for i in range(self.max_y):
+            for j in range(self.max_x):
+                if self.matrix[j][self.max_y -1 -i ] > 0:
+                    r1.append([i,j])
+        for i in range(self.max_y):
+            for j in range(self.max_x):
+                if self.matrix[self.max_x - j -1][i] > 0:
+                    r2.append([i,j])
+        if r1 == r2:
+            rotations.append(r1)
+        else:    
+            rotations.append(r1)
+            rotations.append(r2)
+        return rotations
+
+    def print_f (self):             # DONE TESTED 
+        for i in self.matrix:
+            print(i)
+        print()
+    
+    def print_r (self):             # DONE TESTED 
+        print("---rotations---")
+        for r in self.r0_180:
+            for i in range(self.max_x):
+                for j in range(self.max_y):
+                    if [i,j] in r:
+                        print(self.color, end=" ")
+                    else:
+                        print(0,end=" ")
+                print()
+            print("-----")
+        for r in self.r90_270:
+            for i in range(self.max_y):
+                for j in range(self.max_x):
+                    if [i,j] in r:
+                        print(self.color, end=" ")
+                    else:
+                        print(0,end=" ")
+                print()
+            print("-----")                          
+
+# BOARD
+def create_board_file (name):
+    with open(name, "r") as f:
+        board = list()
+        for i in f:
+            line = list(map(int,i.strip().split()))
+            board.append(line)
+    return board
+
+def create_random_board(size =-1):
+    if size == -1:
+        board_size = random.choice([15])
+    else:
+        board_size = size
+
+    if board_size == 15:
+        marks_count = random.randint(50,50)
+    else:
+        marks_count = random.randint(20,29)
+
+    
+    board = [[0 for _ in range(board_size)] for __ in range(board_size)]
+
+    x = 0
+    xx = True
+    o = 0
+    oo = True
+    while xx or oo:
+        if xx:
+            a = random.randint(0,board_size-1)
+            b = random.randint(0,board_size-1)
+            if board[a][b] == 0:
+                board[a][b] = -1
+                x +=1
+        if oo:
+            a = random.randint(0,board_size-1)
+            b = random.randint(0,board_size-1)
+            if board[a][b] == 0:
+                board[a][b] = 1
+                o +=1
+        if x == marks_count:
+            xx = False
+        if o == marks_count:
+            oo = False
+    
+    return board
+    
+# STONES
+def s_f_p (filename):
+    """ load stones from text file. Stones are stored in the same format as in HW08 """
+    f = open(filename,"r")
+    stones = []
+    for line in f:  
+        nums = list(map(int, line.strip().split()))
+        color = nums[0]
+        coords = nums[1:]
+        cells = []
+        for i in range(0,len(coords)//2):
+            cells.append( [ coords[2*i+0], coords[2*i+1] ] )
+        stones.append([color, cells])
+    f.close()
+    return stones
+
+def read_stones (name):
+    with open (name, "r") as f:
+        stones_data = list()
+        for i in f:
+            stones_data.append(list(map(int, i.strip().split())))
+        stones = list(map(Stone,stones_data,))
+    return stones
+    
+def create_random_stones (amount = -1):
+    if amount == -1:
+        stones_amount = random.choice([17,18,19,20,21,22,23,24,24,24,24])
+    else:
+        stones_amount = amount
+
+
+    possible_stones = [
+        " 0 0 0 1 0 2 1 1 2 1",
+        " 0 0 0 1 0 2 0 3 ",
+        " 0 0 0 1 0 2 1 2",
+        " 0 0 0 1 1 1",
+        " 0 0 0 1 0 2 1 0 1 2",
+        " 0 0 0 1 0 2 1 1",
+        " 0 0 0 1 1 1 2 1 2 2",
+        " 0 0 0 1 0 2",
+        " 0 1 1 1 1 0 1 2 2 1"
+    ]    
+        # # #    # # # #   # # #   # #   #   #   # # #   # #     # # #     # 
+          #                    #     #   # # #     #       #             # # #
+          #                                                # #             #
+
+    stones = list()
+    control_dict = dict()
+    c = 0
+    while c < stones_amount:
+        stone = random.choice(["1","2","3"])
+        stone += random.choice(possible_stones)
+        if control_dict.get(stone):
+            ...
+        else:
+            control_dict[stone] = 1
+            stones.append(stone)
+            c+=1
+    
+    game_stones = list()
+    for i in stones:
+        game_stones.append(list(map(int, i.split())))
+    game_stones = list(map(Stone, game_stones))
+    
+    stones_for_players = list()
+    for i in stones:  
+        nums = list(map(int, i.split()))
+        color = nums[0]
+        coords = nums[1:]
+        cells = []
+        for i in range(0,len(coords)//2):
+            cells.append( [ coords[2*i+0], coords[2*i+1] ] )
+        stones_for_players.append([color, cells])
+
+    return game_stones, stones_for_players
+
+
+colors = {"1":(1,0,0,.8), "2": (.4,.7,.3,.8), "3":(0,0,1,.8), "4": (0,1,1,.8)}
+stones = read_stones("KVHRA\\_stones.txt")
+stones_for_players = s_f_p("KVHRA\\_stones.txt")
---
+++ KVHRA/game_logic.py
@@ -0,0 +1,188 @@
+from PLAYERS.killermode import Player
+
+
+def writeBoard(board, stone, color):
+    for i in range(len(stone)):
+        row, col = stone[i]
+        board[row][col] = color
+
+class Game:
+    def __init__(self, board, stones, player_stones, p1, p2):
+        
+
+        self.board = [[0 for _ in range(len(board))] for __ in range(len(board))]
+        self.marks = board
+        self.stones = stones
+        self.player_stones = player_stones
+
+        # 1 == crosses and starts, -1 == circles go second
+        self.p1 = Player("hozna",self.board,self.marks,self.player_stones,1)
+        self.p1_score = 0
+        self.p2 = Player("pepa",self.board,self.marks,self.player_stones,-1)
+        self.p2_score = 0
+        
+        self.freestones = [True]*len(stones)
+        self.round = 0
+        self.claculate_score()
+
+    def claculate_score (self):
+        circles = 0 
+        crosses = 0
+        for i in self.marks:
+            for j in i:
+                if j == 1:
+                    circles +=1
+                if j == -1:
+                    crosses +=1
+        self.p1_score = circles
+        self.p2_score = crosses
+
+    def check_move (self, x, y, metrix, num, firstmove = False):
+        instructions = list()
+        if 0 > x-len(metrix)+1 or  y+len(metrix[0])-1 >= len(self.board):
+            return False
+
+        for i,j in enumerate(metrix):
+            for k,l in enumerate(j):
+                if l > 0:
+                    instructions.append([i,k])
+
+        color = self.stones[num].color        
+        control =  self.control_base(x-len(metrix)+1,y,instructions,color, firstmove)
+        if control:
+            return True
+        return False
+
+    def human_player_move(self,x,y,matrix,num):
+        for i,j in enumerate(matrix):
+            for k,l in enumerate (j):
+                if l>0:
+                    self.board[x+i][y+k] = l
+                    self.p1.board[x+i][y+k] = l
+                    self.p2.board[x+i][y+k] = l
+                    if self.marks[x+i][y+k] == 1:
+                        self.p1_score -= 1
+                    elif self.marks[x+i][y+k] == -1:
+                        self.p2_score -=1
+        
+        self.freestones[num] = False
+        self.p1.freeStones[num] = False
+        self.p2.freeStones[num] = False
+        self.round +=1
+
+    def p1_move(self):
+        a = self.p1.move()
+        if a:
+            stoneIdx, stone = a
+            stoneColor = self.player_stones[stoneIdx][0]
+            writeBoard(self.p1.board, stone, stoneColor)
+            writeBoard(self.p2.board, stone, stoneColor)
+            writeBoard(self.board, stone, stoneColor)
+            for i,j in stone:
+                if self.marks[i][j] == 1:
+                    self.p1_score -= 1
+                elif self.marks[i][j] == -1:
+                    self.p2_score -=1
+
+            self.p1.freeStones[stoneIdx] = False
+            self.p2.freeStones[stoneIdx] = False
+            self.freestones[stoneIdx] = False
+        else:
+            return None,None,None,None
+        return stone[0][0], stone[0][1], stone, stoneIdx
+
+    def p2_move (self):
+        a = self.p2.move()
+        if a:
+            stoneIdx, stone = a
+            stoneColor = self.player_stones[stoneIdx][0]
+            writeBoard(self.p1.board, stone, stoneColor)
+            writeBoard(self.p2.board, stone, stoneColor)
+            writeBoard(self.board, stone, stoneColor)
+            for i,j in stone:
+                if self.marks[i][j] == 1:
+                    self.p1_score -= 1
+                elif self.marks[i][j] == -1:
+                    self.p2_score -=1
+
+            self.p1.freeStones[stoneIdx] = False
+            self.p2.freeStones[stoneIdx] = False
+            self.freestones[stoneIdx] = False
+        else:
+            return None,None,None,None
+        return stone[0][0], stone[0][1], stone, stoneIdx
+
+# METHODS FOR CONTROL
+    def control_base(self,xx,yy,pos,color, first):
+        control = False
+        added = list()
+        for i,j in pos:
+            x,y = i+xx,yy+j
+            # kontrola jesti se pole vejde, případne vepsání 9
+            if self.board[x][y] > 0:
+                self.delete(added)
+                return False
+            else:
+                self.board[x][y] = 9
+                added.append([x,y])
+
+            # kontrola jesti se neporuší pravidlo o čtevrcích
+            if self.control_rect(x,y):
+                self.delete(added)
+                return False
+
+            if first:
+                control = True
+            # kontrola jestli se neporuší pravidlo barev
+            else:
+                c2 = self.control_color(color,x,y)
+                if c2 == 0:
+                    self.delete(added)
+                    return False
+                elif c2 == 1:
+                    control = True
+        if control:
+            self.delete(added)
+            return True
+        self.delete(added)
+        return False
+    # TRUE = square found
+    # FALSE = test pass 
+    def control_rect(self,x,y):
+        for i in range(-1,1):
+            for j in range(-1,1):
+                c = 0
+                for a,b in [[0,0],[0,1],[1,0],[1,1]]:
+                    
+                    if -1 < x+i+a < len(self.board) and -1 < y+j+b < len(self.board[0]):
+                        if self.board[x+i+a][y+j+b] == 0:
+                            break
+                        else:
+                            c +=1
+                    else: 
+                        break
+                if c == 4:
+                    return True
+        return False
+    # 0 = touch with same color
+    # 1 = touch with different color
+    # 2 = no touch at all
+    def control_color (self, color, x,y):
+        kontola = False
+        for i,j in [[1,0],[-1,0],[0,-1],[0,1]]:
+            if -1 < x+i < len(self.board) and -1 < y+j < len(self.board[0]):
+                if self.board[x+i][y+j] == 0:
+                    ...
+                elif self.board[x+i][y+j] == color:
+                    return  0
+                elif self.board[x+i][y+j] == 9:
+                    ...
+                else:
+                    kontola = True
+        if kontola:
+            return 1
+        return -1
+    # it delletes added "nines" (10th line in control_base)
+    def delete (self, field):#HOTOVO 
+        for x,y in field:
+            self.board[x][y] = 0
